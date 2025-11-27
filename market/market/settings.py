ea2ff14737b05dde9fcc880f022c3e3787f60cdb
MR Nobody <blef7@localhost.localdomain>
2025-11-27 16:51:15 +0300

    update settings.py

--- market/market/settings.py
+++ market/market/settings.py
@@ -115,14 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
